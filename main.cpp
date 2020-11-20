dd24b63fb00cb9f752748738d3fd51e03d6068ad
Egor <wedersgeorge@gmail.com>
2020-11-20 14:13:24 +0700

    )

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
 #include "clSource.h"
 #include <iostream>
 
@@ -22,22 +23,29 @@ int main() {
                     sf::Vertex(sf::Vector2f(size, size), Color(130, 0, 120)),
                     sf::Vertex(sf::Vector2f(size, 0), Color(50, 0, 130)),
             };
-    int key=10;
-  //  cin>>key;
-    Circle *arr=new Circle[key];
-
-        for(int i=0;i<key;i++){
-            if(i<(key/2)){
-                arr[i]=Circle(400,400);
+    int key = 10;
+    //  cin>>key;
+    Circle *arr = new Circle[key];
+    Texture texture;
+    texture.loadFromFile("maxresdefault.jpg");
+    Sprite sprite1(texture);
+    for (int i = 0; i < key; i++) {
+        if (i < (key / 2)) {
+            arr[i] = Circle(400, 400);
         }
-            if(i>(key/2)){
-               arr[i]=Circle((rand()%(size-300)+150),(rand()%(size-300)+150),rand()%100);
-            }
-            if(i==(key/2)){
-               arr[i]=Circle(400,400,(rand()%50),30);
-            }
+        if (i > (key / 2)) {
+            arr[i] = Circle((rand() % (size - 300) + 150), (rand() % (size - 300) + 150), rand() % 100);
+        }
+        if (i == (key / 2)) {
+            arr[i] = Circle(400, 400, (rand() % 50), 30);
+        }
     }
+    Music music;
+    music.openFromFile("red.wav");
+    music.play();
+    music.setVolume(10);
     while (window.isOpen()) {
+
         sf::Event event;
 
         while (window.pollEvent(event)) {
@@ -46,8 +54,12 @@ int main() {
         }
         window.clear();
         window.draw(background, 4, Quads);
+        window.draw(sprite1);
+        sprite1.setScale(Vector2f(0.65, 1.2));
+        sprite1.setColor(Color(255, 255, 255, 64));
         for (int i = 0; i < key; i++) {
             window.draw(arr[i].draw());
+
         }
 
         switch (0) {
@@ -62,5 +74,7 @@ int main() {
                 window.display();
         }
 
+
     }
+    music.stop();
 }
